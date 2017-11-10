cec4e45b0b4fef1841cc114691911ad38510d15d
Jan Vítek <jan.vitek@giant.cz>
2017-11-11 00:28:17 +0100

    Init

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap v4 only grid system | JonathanMH</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="wrapper">
+          <header class="header">
+      <div class="container">
+        <a href="/"><img src="img/logo.svg" alt=""></a>
+      <nav class="nav">
+        <ul>
+          <li>úvod</li>
+          <li>O akademii</li>
+          <li>Co získaš</li>
+          <li>Speakeři</li>
+          <li>Harmonogram</li>
+          <li>Kontakt</li>
+        </ul>
+        <a href="#TODO" class="btn">Přihlásit se</a>
+      </nav>
+      </div>
+    </header>
+
+    <section class="hero">
+      <div class="container">
+        <div class="hero-content">
+          <h1>Mobile Academy</h1>
+          <span class="hero__date">leden 2018, Praha</span>
+          <a href="#TODO" class="btn btn-large">Přihlaš se</a>
+          <ul class="hero__social">
+            <li><a href="#TODO">
+              <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="24px" viewBox="0 0 24.56 23.52"><defs><style>.cls-1{fill:#aaa;}</style></defs><title>Datový zdroj 1</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_linkedin" data-name="icon linkedin" class="cls-1" d="M24.56,14.43v9.09H19.3V15c0-2.14-.76-3.59-2.67-3.59a2.87,2.87,0,0,0-2.7,1.93,3.53,3.53,0,0,0-.18,1.28v8.86H8.49s.07-14.37,0-15.86h5.27V9.91l0,.05h0V9.91A5.23,5.23,0,0,1,18.5,7.29C22,7.29,24.56,9.55,24.56,14.43ZM3,0a2.75,2.75,0,0,0-.47,5.48H3A2.75,2.75,0,0,0,3.39,0ZM.32,23.52H5.58V7.66H.32Z"/></g></g></svg>
+            </a></li>
+           
+            <li><a href="#TODO"><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 23.07 23.13"><defs><style>.cls-1{fill:#aaa;}</style></defs><title>Datový zdroj 1</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_instagram" data-name="icon instagram" class="cls-1" d="M11.55,7.73a3.85,3.85,0,1,1-3.84,3.86h0a3.85,3.85,0,0,1,3.84-3.85m0-1.93a5.78,5.78,0,1,0,5.77,5.8h0A5.77,5.77,0,0,0,11.55,5.8Zm6.15-1.93a1.54,1.54,0,1,1-1.54,1.55h0A1.54,1.54,0,0,1,17.7,3.87h0ZM16.54,1.93a4.63,4.63,0,0,1,4.61,4.63v10a4.62,4.62,0,0,1-4.61,4.63h-10a4.62,4.62,0,0,1-4.61-4.63v-10A4.63,4.63,0,0,1,6.54,1.93h10m0-1.93h-10A6.56,6.56,0,0,0,0,6.55v10a6.56,6.56,0,0,0,6.54,6.55h10a6.56,6.56,0,0,0,6.54-6.55v-10A6.56,6.56,0,0,0,16.54,0Z"/></g></g></svg></a></li>
+            
+            <li><a href="#TODO"><svg xmlns="http://www.w3.org/2000/svg" width="13px" height="23px" viewBox="0 0 12.05 22.58"><defs><style>.cls-1{fill:#aaa;}</style></defs><title>icon facebook</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_facebook" data-name="icon facebook" class="cls-1" d="M11.4,11.29H7.19V22.58H2.34V11.29H0V7.6H2.34V5.2C2.34,1.94,3.75,0,7.59,0H12V4H8.41C7.33,4,7.2,4.56,7.2,5.6v2H12Z"/></g></g></svg></a></li>
+            <li><a href="#TODO"><svg xmlns="http://www.w3.org/2000/svg" width="21px" height="24px" viewBox="0 0 20.84 23.49"><defs><style>.cls-1{fill:#aaa;}</style></defs><title>Datový zdroj 1</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_youtube" data-name="icon youtube" class="cls-1" d="M13.47,9.16a2,2,0,0,0,1.47-.84v.74h1.27V2.38H14.94V7.46a1.35,1.35,0,0,1-.75.5c-.27,0-.34-.18-.34-.45V2.38H12.58V8a1,1,0,0,0,.69,1.17ZM7.8,7.4A1.63,1.63,0,0,0,9.67,9.19,1.73,1.73,0,0,0,11.5,7.57q0-.09,0-.17V4.12a1.77,1.77,0,0,0-1.73-1.8h-.1A1.73,1.73,0,0,0,7.8,4.12ZM9.11,4.23a.55.55,0,1,1,1.09,0V7.34A.55.55,0,0,1,9.74,8H9.67a.57.57,0,0,1-.56-.63V4.23ZM4.76,9.07H6.2V5.39L7.87,0H6.41L5.49,3.62,4.5,0H3.06l1.7,5.39Zm13.16,1.52h-15A2.89,2.89,0,0,0,0,13.44v7.2a2.89,2.89,0,0,0,2.92,2.85h15a2.88,2.88,0,0,0,2.92-2.84h0v-7.2a2.88,2.88,0,0,0-2.91-2.85h0Zm-13,10.77H3.55V14H2.14V12.73H6.33V14H4.92v7.38Zm4.86,0H8.56v-.7a2.6,2.6,0,0,1-.69.59,1,1,0,0,1-1.56-.93V15H7.53v4.88c0,.26.06.43.32.43a1.22,1.22,0,0,0,.71-.48V15H9.78v6.37ZM14.44,20a1.16,1.16,0,0,1-.89,1.37l-.22,0a1.4,1.4,0,0,1-1.15-.57v.49H11V12.73h1.22v2.78a1.47,1.47,0,0,1,1.08-.6c.89,0,1.18.73,1.18,1.6V20Zm4.48-1.72H16.6v1.2c0,.48.05.89.53.89s.55-.34.55-.89v-.44h1.24v.48a1.68,1.68,0,0,1-1.81,2,1.73,1.73,0,0,1-1.75-2V16.71A1.77,1.77,0,0,1,17,14.83h.19a1.63,1.63,0,0,1,1.72,1.87v1.62Zm-1.77-2.38c-.45,0-.55.31-.55.75v.64h1.08v-.64c0-.43-.1-.75-.53-.75Zm-4.72,0a.92.92,0,0,0-.25.19v4a1.15,1.15,0,0,0,.29.22.52.52,0,0,0,.62-.07.72.72,0,0,0,.1-.44V16.58a.76.76,0,0,0-.13-.49.51.51,0,0,0-.63-.1Z"/></g></g></svg></a></li>
+            <li><a href="#TODO"><svg xmlns="http://www.w3.org/2000/svg" width="27px" height="24px" viewBox="0 0 28.1 22.61"><defs><style>.cls-1{fill:#aaa;}</style></defs><title>icon twitter</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_twitter" data-name="icon twitter" class="cls-1" d="M0,20A16.43,16.43,0,0,0,8.84,22.6a16.22,16.22,0,0,0,16.41-16q0-.47,0-.95a11.65,11.65,0,0,0,2.87-3,11.51,11.51,0,0,1-3.31.9A5.73,5.73,0,0,0,27.33.42,11.7,11.7,0,0,1,23.67,1.8,5.8,5.8,0,0,0,19.46,0a5.73,5.73,0,0,0-5.62,7A16.41,16.41,0,0,1,2,1,5.68,5.68,0,0,0,3.74,8.67,5.81,5.81,0,0,1,1.13,8a5.73,5.73,0,0,0,4.62,5.67,5.83,5.83,0,0,1-2.6.1,5.76,5.76,0,0,0,5.39,4A11.69,11.69,0,0,1,0,20Z"/></g></g></svg></a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+    <section class="section about-us">
+      <div class="container">
+        <div class="row">
+        <div class="col-md-6">
+          <h2>O akademii</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur in dolor a congue. Nunc sed nisl nec neque quis dui.</p>
+
+          <ul class="accordion">
+            <li class="accordion__item">
+              <div class="accordion__title">
+                Pro koho je určena?
+              </div>
+              <div class="accordion__content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur in dolor a congue. Nunc sed nisl nec neque quis dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+              </div>
+            </li>
+            <li class="accordion__item">
+              <div class="accordion__title">
+                Pro koho je určena?
+              </div>
+              <div class="accordion__content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur in dolor a congue. Nunc sed nisl nec neque quis dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+              </div>
+            </li>
+            <li class="accordion__item">
+              <div class="accordion__title">
+                Pro koho je určena?
+              </div>
+              <div class="accordion__content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur in dolor a congue. Nunc sed nisl nec neque quis dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      </div>
+    </section>
+
+    <section class="section gain">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3">
+            <div class="text-center">
+              <h2>Co získaš</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur in dolor a congue. Nunc sed nisl nec neque quis dui.</p>
+            </div>
+          </div>
+        </div>
+
+        <ul class="gain-list">
+          <li>
+<svg xmlns="http://www.w3.org/2000/svg" width="36px" height="34px" viewBox="0 0 36 34"><defs><style>.cls-1{fill:#ff5858;}</style></defs><title>Datový zdroj 1</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_book_copy" data-name="icon book copy" class="cls-1" d="M19,30.5H17V5h2ZM19,34H17V33a3,3,0,0,0-3-3H5a5,5,0,0,1-5-5V5A5,5,0,0,1,5,0h9a5,5,0,0,1,5,5H17a3,3,0,0,0-3-3H5A3,3,0,0,0,2,5V25a3,3,0,0,0,3,3h9a5,5,0,0,1,5,5Zm0,0H17V33a5,5,0,0,1,5-5h9a3,3,0,0,0,3-3V5a3,3,0,0,0-3-3H22a3,3,0,0,0-3,3H17a5,5,0,0,1,5-5h9a5,5,0,0,1,5,5V25a5,5,0,0,1-5,5H22a3,3,0,0,0-3,3Z"/></g></g></svg>
+            <span class="gain-list__title">2 měsíce studia</span></li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" width="36px" height="37px" viewBox="0 0 36 36"><defs><style>.cls-1{fill:#ff5858;}</style></defs><title>Datový zdroj 1</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_speaker" data-name="icon speaker" class="cls-1" d="M18,25c-3.23,0-6.67-3.25-8.18-7.65A4,4,0,0,1,7.5,13.72,4.17,4.17,0,0,1,9.07,9.9C9.37,4.91,10.8,0,18,0s8.63,4.91,8.93,9.9a4.16,4.16,0,0,1,1.57,3.81,4,4,0,0,1-2.31,3.64C24.67,21.75,21.23,25,18,25ZM18,2c-4.86,0-6.68,2.25-7,8.6l0,.75-.73.18a2,2,0,0,0-.78,2.05c.09,1.17.71,1.9,1.11,1.91h.73l.21.7C12.8,20.4,15.88,23,18,23s5.2-2.6,6.45-6.8l.21-.7h.73c.4,0,1-.74,1.11-1.92a2,2,0,0,0-.78-2L25,11.35l0-.75C24.68,4.25,22.86,2,18,2Zm0,34C11.33,36,4.82,34.84.59,32.91A1,1,0,0,1,0,32c0-6.46,4-10.66,11.77-12.47l.45,1.94C5.53,23,2.26,26.18,2,31.35,6,33,11.92,34,18,34s12-1,16-2.65c-.25-5.17-3.51-8.32-10.21-9.88l.45-1.94C32,21.34,36,25.54,36,32a1,1,0,0,1-.59.91C31.18,34.84,24.67,36,18,36Z"/></g></g></svg>
+            <span class="gain-list__title">Etn speakery</span></li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" width="34px" height="37px" viewBox="0 0 34 35.99"><defs><style>.cls-1{fill:#ff5858;}</style></defs><title>Datový zdroj 1</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_office" data-name="icon office" class="cls-1" d="M32,36H23a2,2,0,0,1-2-2V21H13V34a2,2,0,0,1-2,2H2a2,2,0,0,1-2-2V13.46a2,2,0,0,1,.74-1.55L14.16,1h0a4.51,4.51,0,0,1,5.68,0L33.26,11.91A2,2,0,0,1,34,13.46V34a2,2,0,0,1-2,2ZM13,19h8a2,2,0,0,1,2,2V34h9V13.46L18.58,2.55a2.52,2.52,0,0,0-3.15,0h0L2,13.46V34h9V21A2,2,0,0,1,13,19Z"/></g></g></svg>
+            <span class="gain-list__title">Kanceláře</span></li>
+          <li>
+          <svg xmlns="http://www.w3.org/2000/svg" width="39px" height="36px" viewBox="0 0 38 35"><defs><style>.cls-1{fill:#ff5858;}</style></defs><title>Datový zdroj 1</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_hardware" data-name="icon hardware" class="cls-1" d="M33,28H5a5,5,0,0,1-5-5V5A5,5,0,0,1,5,0H33a5,5,0,0,1,5,5V23A5,5,0,0,1,33,28ZM5,2A3,3,0,0,0,2,5V23a3,3,0,0,0,3,3H33a3,3,0,0,0,3-3V5a3,3,0,0,0-3-3ZM23,35H15a1,1,0,0,1-.76-.34,1,1,0,0,1-.23-.8l1-7A1,1,0,0,1,16,26h6a1,1,0,0,1,1,.86l1,7a1,1,0,0,1-.23.8A1,1,0,0,1,23,35Zm-6.85-2h5.7l-.72-5H16.87ZM26,35H12a1,1,0,0,1,0-2H26a1,1,0,0,1,0,2ZM37,22H1V20H37Z"/></g></g></svg>
+          <span class="gain-list__title">hardware</span></li>
+          <li>
+          <svg xmlns="http://www.w3.org/2000/svg" width="22px" height="36px" viewBox="0 0 22 36"><defs><style>.cls-1{fill:#ff5858;}</style></defs><title>Datový zdroj 1</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_phone" data-name="icon phone" class="cls-1" d="M17,36H5a5,5,0,0,1-5-5V5A5,5,0,0,1,5,0H17a5,5,0,0,1,5,5V31A5,5,0,0,1,17,36ZM5,2A3,3,0,0,0,2,5V31a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V5a3,3,0,0,0-3-3Zm6,31a2,2,0,1,1,2-2A2,2,0,0,1,11,33Zm0-2ZM13,5H9A1,1,0,0,1,9,3h4a1,1,0,0,1,0,2Z"/></g></g></svg>
+          <span class="gain-list__title">iOS základy</span></li>
+        </ul>
+      </div>
+    </section>
+
+
+    <section class="section speakers">
+      <div class="container">
+        <h2>Speakeři</h2>
+        <ul>
+          <li>
+            <div class="row">
+              <div class="col-md-3">
+                <img src="img/speakers/milos-felix.jpg" alt="">
+              </div>
+              <div class="col-md-8 col-offset-md-1">
+                <h3>
+                  Mobile Division Leader
+                  Miloš felix
+                </h3>
+                <p>We feel that the Czech pool of mobile developers does not currently have a unified community that would meet regularly, bond, share true know-how and mutually move ahead. </p>
+                <ul class="speakers-social">
+                  <li><a href="#TODO"><svg xmlns="http://www.w3.org/2000/svg" width="19px" height="20px" viewBox="0 0 23.07 23.13"><defs><style>.cls-1{fill:#aaa;}</style></defs><title>Datový zdroj 1</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_instagram" data-name="icon instagram" class="cls-1" d="M11.55,7.73a3.85,3.85,0,1,1-3.84,3.86h0a3.85,3.85,0,0,1,3.84-3.85m0-1.93a5.78,5.78,0,1,0,5.77,5.8h0A5.77,5.77,0,0,0,11.55,5.8Zm6.15-1.93a1.54,1.54,0,1,1-1.54,1.55h0A1.54,1.54,0,0,1,17.7,3.87h0ZM16.54,1.93a4.63,4.63,0,0,1,4.61,4.63v10a4.62,4.62,0,0,1-4.61,4.63h-10a4.62,4.62,0,0,1-4.61-4.63v-10A4.63,4.63,0,0,1,6.54,1.93h10m0-1.93h-10A6.56,6.56,0,0,0,0,6.55v10a6.56,6.56,0,0,0,6.54,6.55h10a6.56,6.56,0,0,0,6.54-6.55v-10A6.56,6.56,0,0,0,16.54,0Z"/></g></g></svg></a></li>
+                  <li><a href="#TODO"><svg xmlns="http://www.w3.org/2000/svg" width="10px" height="20px" viewBox="0 0 12.05 22.58"><defs><style>.cls-1{fill:#aaa;}</style></defs><title>icon facebook</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_facebook" data-name="icon facebook" class="cls-1" d="M11.4,11.29H7.19V22.58H2.34V11.29H0V7.6H2.34V5.2C2.34,1.94,3.75,0,7.59,0H12V4H8.41C7.33,4,7.2,4.56,7.2,5.6v2H12Z"/></g></g></svg></a></li>
+                  <li><a href="#TODO"><svg xmlns="http://www.w3.org/2000/svg" width="21px" height="20px" viewBox="0 0 24.56 23.52"><defs><style>.cls-1{fill:#aaa;}</style></defs><title>Datový zdroj 1</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_linkedin" data-name="icon linkedin" class="cls-1" d="M24.56,14.43v9.09H19.3V15c0-2.14-.76-3.59-2.67-3.59a2.87,2.87,0,0,0-2.7,1.93,3.53,3.53,0,0,0-.18,1.28v8.86H8.49s.07-14.37,0-15.86h5.27V9.91l0,.05h0V9.91A5.23,5.23,0,0,1,18.5,7.29C22,7.29,24.56,9.55,24.56,14.43ZM3,0a2.75,2.75,0,0,0-.47,5.48H3A2.75,2.75,0,0,0,3.39,0ZM.32,23.52H5.58V7.66H.32Z"/></g></g></svg></a></li>
+                  <li><a href="#TODO"><svg xmlns="http://www.w3.org/2000/svg" width="22px" height="20px" viewBox="0 0 28.1 22.61"><defs><style>.cls-1{fill:#aaa;}</style></defs><title>icon twitter</title><g id="Vrstva_2" data-name="Vrstva 2"><g id="Vrstva_1-2" data-name="Vrstva 1"><path id="icon_twitter" data-name="icon twitter" class="cls-1" d="M0,20A16.43,16.43,0,0,0,8.84,22.6a16.22,16.22,0,0,0,16.41-16q0-.47,0-.95a11.65,11.65,0,0,0,2.87-3,11.51,11.51,0,0,1-3.31.9A5.73,5.73,0,0,0,27.33.42,11.7,11.7,0,0,1,23.67,1.8,5.8,5.8,0,0,0,19.46,0a5.73,5.73,0,0,0-5.62,7A16.41,16.41,0,0,1,2,1,5.68,5.68,0,0,0,3.74,8.67,5.81,5.81,0,0,1,1.13,8a5.73,5.73,0,0,0,4.62,5.67,5.83,5.83,0,0,1-2.6.1,5.76,5.76,0,0,0,5.39,4A11.69,11.69,0,0,1,0,20Z"/></g></g></svg></a></li>
+                </ul>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <ul class="speakers-list">
+        <li><span class="speakers-list__name">Miloš felix</span></li>
+        <li><span class="speakers-list__name">Le kien</span></li>
+        <li><span class="speakers-list__name">Číslinský ján</span></li>
+        <li><span class="speakers-list__name">Franek ondřej</span></li>
+        <li><span class="speakers-list__name">Bien Radek</span></li>
+      </ul>
+    </section>
+
+    
+    <section class="section harmonogram">
+      <div class="container">
+        <h2>Harmonogram</h2>
+        <ul class="harmonogram__list">
+          <li>
+            <a href="#TODO">
+              <span class="harmonogram__title">Registrace</span>
+              <span class="harmonogram__number"><strong>10</strong>/2017</span>
+              <p>Nunc sed nisl nec neque quis dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            </a>
+          </li>
+          <li>
+            <a href="#TODO">
+              <span class="harmonogram__title">Ukončení registrace</span>
+              <span class="harmonogram__number"><strong>11</strong>/2017</span>
+              <p>Nunc sed nisl nec neque quis dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            </a>
+          </li>
+          <li>
+            <a href="#TODO">
+              <span class="harmonogram__title">Selekce kandidátů</span>
+              <span class="harmonogram__number"><strong>12</strong>/2017</span>
+              <p>Nunc sed nisl nec neque quis dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            </a>
+          </li>
+          <li>
+            <a href="#TODO">
+              <span class="harmonogram__title">Akademie jede!</span>
+              <span class="harmonogram__number"><strong>01</strong>/2018</span>
+              <p>Nunc sed nisl nec neque quis dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            </a>
+          </li>
+          <li>
+            <a href="#TODO">
+              <span class="harmonogram__title">Akademie jede</span>
+              <span class="harmonogram__number"><strong>01-02</strong>/2018</span>
+              <p>Nunc sed nisl nec neque quis dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            </a>
+          </li>
+          <li>
+            <a href="#TODO">
+              <span class="harmonogram__title">Promoce</span>
+              <span class="harmonogram__number"><strong>03</strong>/2018</span>
+              <p>Nunc sed nisl nec neque quis dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="section contact">
+      <div class="container">
+        <h2>Kontakt</h2>
+        <h3>Etnetera a.s.</h3>
+        <ul>
+          <li>
+            <span class="contact__icon"><img src="/img/icon-phone-2.svg" alt=""></span>
+            +420 233 326 810
+          </li>
+          <li>
+            <span class="contact__icon"><img src="/img/icon-place.svg" alt=""></span>
+            Jankovcova 1037/49<br>
+            170 00 Praha 7
+          </li>
+          <li>
+            <span class="contact__icon"><img src="/img/icon-mail.svg" alt=""></span>
+            <a href="#TODO">info@etnetera.cz</a>
+          </li>
+        </ul>
+        <a href="#TODO" class="btn">Podívejte se na mapu</a>
+      </div>
+    </section>
+
+    <footer class="footer">
+      <div class="container">
+        <p>Nenašel jsi odpovědi na to, co jsi hledal?</p>
+        <a href="#TODO" class="btn btn--white">Napiš nám</a>
+      </div>
+    </footer>
+
+    </div>
+  </body>
+</html>
